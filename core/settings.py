6cfc2133d11d92e656dbf552115d3a51f76dc178
Dmytro <94nj111@gmail.com>
2024-11-29 13:52:56 +0200

    add templates & static dirs, settings + styles.css

--- core/settings.py
+++ core/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = "core.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -67,6 +67,12 @@ TEMPLATES = [
             ],
         },
     },
+]
+
+STATIC_URL = "static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
 ]
 
 WSGI_APPLICATION = "core.wsgi.application"
