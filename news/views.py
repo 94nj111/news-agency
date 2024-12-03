c6333c3a76efbd4bfa613b9c17db1994425ffe36
Dmytro <94nj111@gmail.com>
2024-12-03 10:39:32 +0200

    style improve

--- news/views.py
+++ news/views.py
@@ -12,7 +12,7 @@ class NewspaperListView(generic.ListView):
     queryset = Newspaper.objects.prefetch_related(
         "topics", "publishers"
     ).order_by("-published_date")
-    paginate_by = 2
+    paginate_by = 5
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(NewspaperListView, self).get_context_data(**kwargs)
